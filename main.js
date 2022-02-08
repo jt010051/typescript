37aa6e79b5e451ac1add1f954b62cd8f3f78e374
jt010051 <jont26.smith@hcl.com>
2022-02-08 15:41:20 -0500

    enum

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-function doSomething() {
-    for (var i = 0; i < 5; i++) {
-        console.log(i);
-    }
-    console.log('Finally: ' + i);
-}
-doSomething();
+var a;
+var e = [1, 2, 3];
+var f = [1, true, 'a', false];
+var ColorRed = 0;
+var ColorGreen = 1;
+var ColorBlue = 2;
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Blue"] = 1] = "Blue";
+    Color[Color["Green"] = 2] = "Green";
+    Color[Color["Purpule"] = 3] = "Purpule";
+})(Color || (Color = {}));
+;
+var backgroundColor = Color.Red;
