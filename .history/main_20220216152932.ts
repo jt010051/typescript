a9a7be3d8a3aa70ed85c1850cff941aeb65e7a01
jt010051 <jont26.smith@hcl.com>
2022-02-16 15:37:27 -0500

    Initial Properties commit

--- .history/main_20220216152932.ts
+++ .history/main_20220216152932.ts
@@ -1,25 +1,22 @@
 class Point{//class
 
-constructor(private _x?: number, private _y?: number){ //? Optional parameters (Typescript)
+constructor(private x?: number, private y?: number){ //? Optional parameters (Typescript)
 //access modifiers generate fields with exact same name and initializes field with valueless argument (Typescript)
 }
 draw (){
-console.log('X: '+this._x+ ', Y: ' +this._y)
+console.log('X: '+this.x+ ', Y: ' +this.y)
 
+}getX(){
+    return this.x;
 }
-get x(){
-    return this._x;
-}
-set x(value){
+setX(value){
     if(value<0){
 throw new Error('value cannot be less than 0');
-this._x=value;
     }
 }
 }
 
 
 let point =new Point(1, 2);
-let x = point.X;
-point.X=10;
+let x = point.getX;
 point.draw();
